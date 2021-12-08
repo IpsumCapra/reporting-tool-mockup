e7adf5036425dd0b4cd058fdc538e65e043a9558
IpsumCapra <samuelcornelisse@gmail.com>
2021-12-08 15:13:06 +0100

    Added home blade and language files.

---
+++ resources/lang/en/home.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    // Home page
+    'title' => 'Home',
+    'header_auth' => 'Welcome :user.firstname!',
+    'header_guest' => 'Welcome on the Secura Reporting Tool Mockup',
+    'description' => 'This tool is used for reporting, and consolidating the results of research Secura conducts for her customers.',
+];
