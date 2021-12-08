75164bb40f9f6a4e04af38b8902ebf39382aa09b
IpsumCapra <samuelcornelisse@gmail.com>
2021-12-08 15:12:47 +0100

    Added layout blade and language files.

---
+++ resources/lang/en/layout.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    // Metadata
+    'description' => 'Reporting tool mockup for third year internship of computer science engineering.',
+
+    // Layout header
+    'header.admin.home' => 'Admin',
+    'header.admin.users' => 'Users',
+    'header.settings' => 'Settings',
+    'header.logout' => 'Logout',
+    'header.login' => 'Login',
+
+    // Layout footer
+    'footer.authors' => 'Made by Sam Cornelisse for third year internship',
+    'footer.source' => 'For the source code go to <a href="https://github.com/IpsumCapra/reporting-tool-mockup" target="_blank" rel="noreferrer">GitHub</a>',
+];
