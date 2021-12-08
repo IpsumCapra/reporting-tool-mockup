c1e998e4a28e039549de4da4f1634e68ba6f85b6
IpsumCapra <samuelcornelisse@gmail.com>
2021-12-08 16:51:50 +0100

    Added standard admin user in database seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,5 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
+
+
+        DB::table('users')->insert([
+            'firstname' => "Admin",
+            'lastname' => "Admin",
+            'gender' => User::GENDER_MALE,
+            'birthday' => $faker->date(),
+            'email' => "admin@a.a",
+            'address' => "Plekstad 123",
+            'postcode' => "1234AB",
+            'city' => "Plek",
+            'province' => User::PROVINCES[0],
+            'password' => Hash::make("admin"),
+            'role' => User::ROLE_ADMIN,
+        ]);
     }
 }
