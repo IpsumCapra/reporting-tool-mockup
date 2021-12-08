e930d7ca91ea68d2170d79c826e1d9f6b0812bba
IpsumCapra <samuelcornelisse@gmail.com>
2021-12-08 15:16:14 +0100

    Implemented routing, removed redundant welcome page.

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Home page
+Route::view('/', 'home')->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
+// Normal routes
+Route::middleware('auth')->group(function () {
+    // Settings
+    Route::view('/settings', 'settings')->name('settings');
+    Route::post('/settings/change_details', [SettingsController::class, 'changeDetails'])->name('settings.change_details');
+    Route::post('/settings/change_password', [SettingsController::class, 'changePassword'])->name('settings.change_password');
+
+    // Auth logout
+    Route::get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
+
+// Guest routes
+Route::middleware('guest')->group(function () {
+    // Auth login
+    Route::view('/auth/login', 'auth.login')->name('auth.login');
+    Route::post('/auth/login', [AuthController::class, 'login']);
+
+    // Auth register
+    Route::view('/auth/register', 'auth.register')->name('auth.register');
+    Route::post('/auth/register', [AuthController::class, 'register']);
 });
