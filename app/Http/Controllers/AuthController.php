8f79300a800c3c3478af69b2accf75a910dda45f
IpsumCapra <samuelcornelisse@gmail.com>
2021-12-08 15:15:40 +0100

    Added site controllers.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hospital;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Session;
+
+class AuthController extends Controller
+{
+    // Login route
+    public function login(Request $request)
+    {
+        // Validate input
+        $fields = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Try to login when successful go to home page
+        if (
+        Auth::attempt([
+            'email' => $fields['email'],
+            'password' => $fields['password']
+        ], true)
+        ) {
+            return redirect()->route('home');
+        }
+
+        // When not successfull go back with error
+        return back()->withInput()->with('error', __('auth.login.error'));
+    }
+
+    // Logout route
+    public function logout()
+    {
+        // Logout user
+        Session::flush();
+        Auth::logout();
+
+        // Go to login page
+        return redirect()->route('auth.login');
+    }
+}
---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+
+class SettingsController extends Controller
+{
+    // Change details route
+    public function changeDetails(Request $request)
+    {
+        // Validate input
+        $fields = $request->validate([
+            'firstname' => 'required|min:2',
+            'insertion' => 'nullable',
+            'lastname' => 'required|min:2',
+            'gender' => 'required|integer|digits_between:' . User::GENDER_MALE . ',' . User::GENDER_OTHER,
+            'birthday' => 'required|date',
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users')->ignore(Auth::user()->email, 'email')
+            ],
+            'phone' => 'nullable',
+            'address' => 'required|min:2',
+            'postcode' => 'required|min:2',
+            'city' => 'required|min:2',
+            'country' => 'required|min:2',
+        ]);
+
+        // Update user details
+        Auth::user()->update([
+            'firstname' => $fields['firstname'],
+            'insertion' => $fields['insertion'],
+            'lastname' => $fields['lastname'],
+            'gender' => $fields['gender'],
+            'birthday' => $fields['birthday'],
+            'email' => $fields['email'],
+            'phone' => $fields['phone'],
+            'address' => $fields['address'],
+            'postcode' => $fields['postcode'],
+            'city' => $fields['city'],
+            'country' => $fields['country']
+        ]);
+
+        // Go back with message
+        return back()->with('message', __('settings.change_details_message'));
+    }
+
+    // Change password route
+    public function changePassword(Request $request)
+    {
+        // Validate input
+        $fields = $request->validate([
+            'current_password' => function ($attribute, $value, $fail) {
+                if (!Hash::check($value, Auth::user()->password)) {
+                    $fail(__('validation.current_password', ['attribute' => $attribute]));
+                }
+            },
+            'password' => 'required|min:6'
+        ]);
+
+        // Update user password
+        Auth::user()->update([
+            'password' => Hash::make($fields['password'])
+        ]);
+
+        // Go back with message
+        return back()->with('message', __('settings.change_password_message'));
+    }
+}
