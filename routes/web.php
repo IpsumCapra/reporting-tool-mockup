ec4d3bf48888e35ccfc06c22bd267ebdce4bde91
IpsumCapra <samuelcornelisse@gmail.com>
2021-12-08 17:49:06 +0100

    Added admin routes.

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,22 @@ Route::middleware('auth')->group(function () {
     Route::get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
 
+// Admin routes
+Route::middleware('admin')->group(function () {
+    // Admin home
+    Route::view('/admin', 'admin.home')->name('admin.home');
+
+    // Admin user routes
+    Route::get('/admin/users', [AdminUsersController::class, 'index'])->name('admin.users.index');
+    Route::view('/admin/users/create', 'admin.users.create')->name('admin.users.create');
+    Route::post('/admin/users', [AdminUsersController::class, 'store'])->name('admin.users.store');
+    Route::get('/admin/users/{user}/hijack', [AdminUsersController::class, 'hijack'])->name('admin.users.hijack');
+    Route::get('/admin/users/{user}/edit', [AdminUsersController::class, 'edit'])->name('admin.users.edit');
+    Route::get('/admin/users/{user}/delete', [AdminUsersController::class, 'delete'])->name('admin.users.delete');
+    Route::get('/admin/users/{user}', [AdminUsersController::class, 'show'])->name('admin.users.show');
+    Route::post('/admin/users/{user}', [AdminUsersController::class, 'update'])->name('admin.users.update');
+});
+
 // Guest routes
 Route::middleware('guest')->group(function () {
     // Auth login
